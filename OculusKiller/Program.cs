ba56c50c41f0249dfca1068e016ab2deb2dd7bac
Eliminater74 <eliminater74@gmail.com>
2023-09-18 16:08:13 -0400

    *** Fix Comiler Warning & comment code ***
    Code speaks for it self

--- OculusKiller/Program.cs
+++ OculusKiller/Program.cs
@@ -9,15 +9,18 @@ namespace OculusKiller
 {
     public class Program
     {
+        // Define the path for logging
         private static string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "OculusKiller", "logs.txt");
 
         public static async Task Main()
         {
             try
             {
+                // Initialize the logging system
                 InitializeLogging();
                 Log("Application started.");
 
+                // Get paths for Oculus and SteamVR
                 string oculusPath = GetOculusPath();
                 var result = GetSteamPaths();
                 if (result == null || String.IsNullOrEmpty(oculusPath))
@@ -27,6 +30,7 @@ namespace OculusKiller
                 string startupPath = result.Item1;
                 string vrServerPath = result.Item2;
 
+                // Retry mechanism for starting SteamVR
                 const int maxRetries = 2;
                 const int delayBetweenRetries = 2000; // 2 seconds
                 bool processStarted = false;
@@ -61,6 +65,7 @@ namespace OculusKiller
                     return;
                 }
 
+                // Monitor the processes to ensure proper shutdown
                 MonitorProcesses(oculusPath, vrServerPath);
             }
             catch (Exception e)
@@ -70,6 +75,7 @@ namespace OculusKiller
             }
         }
 
+        // Initialize the logging directory
         private static void InitializeLogging()
         {
             if (!Directory.Exists(Path.GetDirectoryName(logPath)))
@@ -78,11 +84,13 @@ namespace OculusKiller
             }
         }
 
+        // Log messages to the defined log path
         private static void Log(string message)
         {
             File.AppendAllText(logPath, DateTime.Now + ": " + message + "\n");
         }
 
+        // Monitor Oculus and SteamVR processes for proper shutdown
         private static void MonitorProcesses(string oculusPath, string vrServerPath)
         {
             try
@@ -96,7 +104,7 @@ namespace OculusKiller
                 }
 
                 vrServerProcess.EnableRaisingEvents = true;
-                vrServerProcess.Exited += async (sender, e) =>
+                vrServerProcess.Exited += (sender, e) =>
                 {
                     Log("SteamVR vrserver exited.");
                     Process ovrServerProcess = GetProcessByNameAndPath("OVRServer_x64", oculusPath);
@@ -120,11 +128,13 @@ namespace OculusKiller
             }
         }
 
+        // Get a specific process by its name and path
         private static Process GetProcessByNameAndPath(string processName, string processPath)
         {
             return Array.Find(Process.GetProcessesByName(processName), process => process.MainModule.FileName == processPath);
         }
 
+        // Get the path for Oculus
         static string GetOculusPath()
         {
             string oculusPath = Environment.GetEnvironmentVariable("OculusBase");
@@ -144,6 +154,7 @@ namespace OculusKiller
             return oculusPath;
         }
 
+        // Get the paths for SteamVR
         public static Tuple<string, string> GetSteamPaths()
         {
             string openVrPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"openvr\openvrpaths.vrpath");
